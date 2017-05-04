be56b1ac728b4a22fcfce942ac4a0979f8457393
owlas <ollielaslett@icloud.com>
2017-05-04 10:08:57 +0100

    add regression test for sklearn version

--- tests/test.py
+++ tests/test.py
@@ -7,15 +7,14 @@ except ImportError:
     sys.exit(0)
 
 def test_python3():
-    
     assert sys.version_info.major == 3
 
-    
 def test_pandas():
     import pandas
 
 def test_sklearn():
     import sklearn
+    from sklearn.model_selection import train_test_split
 
 def test_matplotlib():
     import matplotlib
